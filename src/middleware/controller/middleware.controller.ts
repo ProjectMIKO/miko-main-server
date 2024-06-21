18fa06f043935fbfb2822bbeaf9917743c302bae
Arklimits <Arklimits@users.noreply.github.com>
2024-06-21 22:59:08 +0900

    #16 feat: STT Middleware 서비스 구현

--- src/middleware/controller/middleware.controller.ts
+++ src/middleware/controller/middleware.controller.ts
@@ -1,8 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { MiddlewareService } from '../service/middleware.service';
 import { SummarizeRequestDto } from '../dto/summarize.request.dto';
-import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SummarizeResponseDto } from '../dto/summarize.response.dto';
+import { UploadResponseDto } from '../../filesystem/dto/upload.response.dto';
+import { Express } from 'express';
+import { ConvertResponseDto } from '../dto/convert.response.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @ApiTags('middleware')
 @Controller('api/middleware')
@@ -18,7 +22,38 @@ export class MiddlewareController {
   })
   @ApiResponse({ status: 400, description: 'Bad Request' })
   @ApiResponse({ status: 500, description: 'Internal Server Error' })
-  async summarizeText(@Body() summarizeRequestDto: SummarizeRequestDto): Promise<SummarizeResponseDto> {
+  async summarizeText(
+    @Body() summarizeRequestDto: SummarizeRequestDto,
+  ): Promise<SummarizeResponseDto> {
     return this.middlewareService.summarizeText(summarizeRequestDto);
+  }
+
+  @Post('stt')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Audio file to upload',
+    required: true,
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The file has been successfully uploaded.',
+    type: UploadResponseDto,
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 500, description: 'Internal Server Error' })
+  async convertStt(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<ConvertResponseDto> {
+    return await this.middlewareService.convertStt(file);
   }
 }

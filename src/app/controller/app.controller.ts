7692af57003c8898fb96660536d25dd0ec5ece95
Arklimits <Arklimits@users.noreply.github.com>
2024-06-25 23:07:22 +0900

    fix: favicon 업로드

--- src/app/controller/app.controller.ts
+++ src/app/controller/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Redirect, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from '../gateway/app.gateway';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('favicon.ico')
+  sendFavicon(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', '..', 'asset', 'public', 'favicon', 'favicon.ico'));
+  }
 
   @Get()
   getHello(): string {

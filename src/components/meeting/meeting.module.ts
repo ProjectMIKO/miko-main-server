02fbb039f07fd62ec880285bdecd465d28857c7f
Arklimits <Arklimits@users.noreply.github.com>
2024-06-29 13:15:46 +0900

    test: 하부 레이어 Exception Filter 추가

--- src/components/meeting/meeting.module.ts
+++ src/components/meeting/meeting.module.ts
@@ -3,13 +3,13 @@ import { MeetingService } from './service/meeting.service';
 import { MeetingController } from './controller/meeting.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Meeting, MeetingSchema } from './schema/meeting.schema';
+import { GlobalExceptionsFilter } from '@global/filter/global.exceptions.filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Meeting.name, schema: MeetingSchema }]),
-  ],
+  imports: [MongooseModule.forFeature([{ name: Meeting.name, schema: MeetingSchema }])],
   controllers: [MeetingController],
-  providers: [MeetingService],
+  providers: [MeetingService, { provide: APP_FILTER, useClass: GlobalExceptionsFilter }],
   exports: [MeetingService],
 })
 export class MeetingModule {}
